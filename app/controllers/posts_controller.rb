4334bc4cc537a0f92dc2d3dd4aea1d044e5b7a44
yusup84 <globalinformatika84@gmail.com>
2023-11-21 07:02:51 +0700

    Finish implementation of blog functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,49 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+
+    if @post
+      redirect_to @post
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      redirect_to "edit"
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
